d96663ec98d0c979b12bb771c16a7d943d7a47c5
shehanpeiris <shehanpeiris@gmail.com>
2019-04-20 12:44:21 -0500

    tweaked styling of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,8 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-        React Recipes
-      </a>
+      <h1 style={{color: "#ffffff"}}>The Memory Memory Game</h1>
+      <h3 style={{color: "#ffffff"}}>Current Score:   ||  High Score:  </h3>
     </nav>
   );
 }
