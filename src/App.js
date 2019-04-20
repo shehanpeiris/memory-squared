875b31475a762cb5c1160b784f88b1a54b85bad1
shehanpeiris <shehanpeiris@gmail.com>
2019-04-20 12:36:26 -0500

    navbar and wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
+  
+  
+  
+  
+  
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Navbar></Navbar>
+        <Wrapper>
+        </Wrapper>
+      </Wrapper>
+      
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        React Recipes
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
