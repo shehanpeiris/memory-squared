6c65835355a46fe650f5f410a61b6f0e02a5bbfb
shehanpeiris <shehanpeiris@gmail.com>
2019-04-28 15:46:35 -0500

    functional game

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./Jumbotron.css";
+
 
 function Jumbotron (props) {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
         <h1 className="display-2 text-center">The Memory Memory Game</h1>
-        <h1 className="display-4 text-center">How does your memory compare to some of the best memories in history?</h1>
-        <h1 className="display-4 text-center">Current Score: {props.currentScore}</h1>
-        <h1 className="display-4 text-center">High Score: {props.highScore} </h1>
+        <h1 className="display-4 text-center subheading">How does your memory compare to some of the best memories in history?</h1>
+        <h1 className="display-4 text-center scoreboard">Current Score: {props.currentScore} | High Score: {props.highScore}</h1>
+        {/* <h1 className="display-4 text-center">High Score: {props.highScore} </h1> */}
         <p className="lead">How it works: Click on each of the 12 memory specialists shown below.</p>
       </div>
     </div>
