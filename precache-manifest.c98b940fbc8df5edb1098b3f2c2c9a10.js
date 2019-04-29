8236440f1d9e155374077b5459070605adf1bced
shehanpeiris <shehanpeiris@gmail.com>
2019-04-29 16:57:08 -0500

    Updates

---
+++ precache-manifest.c98b940fbc8df5edb1098b3f2c2c9a10.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "67926268e02fdb1d0bd655335d9ec8cb",
+    "url": "/memory-squared/static/media/bran_stark.67926268.jpg"
+  },
+  {
+    "revision": "b1912f13c1a1e9002ef9",
+    "url": "/memory-squared/static/css/main.6e50e35e.chunk.css"
+  },
+  {
+    "revision": "32e2e022c6923d602d25",
+    "url": "/memory-squared/static/js/runtime~main.202fcd71.js"
+  },
+  {
+    "revision": "0ea650d6859aad342f7d",
+    "url": "/memory-squared/static/js/2.95fdad0e.chunk.js"
+  },
+  {
+    "revision": "1727455ac7e1af59d4b4a5f9035925eb",
+    "url": "/memory-squared/static/media/lebron_james.1727455a.jpg"
+  },
+  {
+    "revision": "2bef92a12be904cdb21f53de74f7b2d9",
+    "url": "/memory-squared/static/media/sherlock_holmes.2bef92a1.jpg"
+  },
+  {
+    "revision": "63ec17365377d2361df171764ff4960a",
+    "url": "/memory-squared/static/media/nikola_tesla.63ec1736.jpg"
+  },
+  {
+    "revision": "400eb1dcd691c28879d815f741c6b1fe",
+    "url": "/memory-squared/static/media/teddy_roosevelt.400eb1dc.jpg"
+  },
+  {
+    "revision": "b1912f13c1a1e9002ef9",
+    "url": "/memory-squared/static/js/main.cc4e82d1.chunk.js"
+  },
+  {
+    "revision": "182f20968f2ad4b00c9367746cfbb7cc",
+    "url": "/memory-squared/static/media/david_boies.182f2096.jpg"
+  },
+  {
+    "revision": "1a1d1faf51fb0f950ee56bb582083cd7",
+    "url": "/memory-squared/static/media/guillermo_deltoro.1a1d1faf.jpg"
+  },
+  {
+    "revision": "46c73422c2bb5b0ebbf2aefdcde11a46",
+    "url": "/memory-squared/static/media/jerry_lucas.46c73422.jpg"
+  },
+  {
+    "revision": "aa6d83689c30e457dbd835d10926b5bd",
+    "url": "/memory-squared/static/media/kim_peek.aa6d8368.jpeg"
+  },
+  {
+    "revision": "16abe4f8030d4ecc3236ba007c014380",
+    "url": "/memory-squared/static/media/leonhard_euler.16abe4f8.jpg"
+  },
+  {
+    "revision": "3633f1fd5829f95bf6cce4252717bb60",
+    "url": "/memory-squared/static/media/sean_mcvay.3633f1fd.jpg"
+  },
+  {
+    "revision": "f4e93086b33f5eb002a29aab029cc124",
+    "url": "/memory-squared/static/media/sergei_rachmaninoff.f4e93086.png"
+  },
+  {
+    "revision": "11e1775a5b6bfe2ad8162cd4de399f79",
+    "url": "/memory-squared/index.html"
+  }
+];
