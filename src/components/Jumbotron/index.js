559464f783e75a55cc6a2db6e47859929306a3f6
shehanpeiris <shehanpeiris@gmail.com>
2019-04-29 17:05:10 -0500

    updated readme and removed redundant code

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -9,8 +9,6 @@ function Jumbotron (props) {
         <h1 className="display-2 text-center">The Memory Memory Game</h1>
         <h1 className="display-4 text-center subheading">How does your memory compare to some of the best memories in history?</h1>
         <h1 className="display-4 text-center scoreboard">Current Score: {props.currentScore} | High Score: {props.highScore}</h1>
-        {/* <h1 className="display-4 text-center">High Score: {props.highScore} </h1> */}
-        <p className="lead">How it works: Click on each of the 12 memory specialists shown below.</p>
       </div>
     </div>
   )
